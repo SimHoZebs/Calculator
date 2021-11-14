57369c229f9d0b4f6331a3afa84956ea3883b19a
SimHoZebs <simhozebs@gmail.com>
2021-11-14 13:21:05 +0900

    Added Color Mode Toggle - removed .js.map files

--- lib/Button.tsx
+++ lib/Button.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Button = (props: Props) => {
   return (
-    <button className="bg-gray-700 text-gray-100 h-20 w-20 rounded-full text-4xl">
+    <button className="bg-gray-500 dark:bg-gray-800 text-gray-900 dark:text-gray-100 h-20 w-20 rounded-full text-4xl">
       {props.children}
     </button>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import Button from "../lib/Button"
+import ColorModeToggleBtn from "../lib/ColorModeToggleBtn"
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen bg-gray-100 dark:bg-gray-700 p-2">
-      <div className="h-1/3">Top </div>
+    <div className="h-screen w-screen bg-gray-100 dark:bg-gray-900 p-2">
+      <div className="h-1/3">
+        <div className="flex justify-end">
+          <ColorModeToggleBtn />
+        </div>
+      </div>
       <div className="h-2/3">
         <Button>1</Button>
       </div>
