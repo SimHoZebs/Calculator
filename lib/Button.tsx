a7f59acdbefda4a408884757e4d581eaa33f0df1
SimHoZebs <simhozebs@gmail.com>
2021-11-14 16:42:34 +0900

    Added operator keys

--- lib/Button.tsx
+++ lib/Button.tsx
@@ -2,11 +2,22 @@ import React from "react"
 
 interface Props {
   children: React.ReactNode
+  isOperator?: boolean
 }
 
 const Button = (props: Props) => {
+  const numKeyColor =
+    "bg-gray-500 dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+
+  const operatorColor =
+    "bg-blue-800 dark:bg-blue-500 text-gray-100 dark:text-gray-900"
+
   return (
-    <button className="bg-gray-500 dark:bg-gray-800 text-gray-900 dark:text-gray-100 h-20 w-20 rounded-full text-4xl">
+    <button
+      className={`${
+        props.isOperator ? operatorColor : numKeyColor
+      } h-20 w-20 rounded-full text-4xl`}
+    >
       {props.children}
     </button>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,9 @@ import Button from "../lib/Button"
 import ColorModeToggleBtn from "../lib/ColorModeToggleBtn"
 
 export default function Home() {
+  const numArray = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, ".", "DEL"]
+  const topOperatoryArray = ["AC", "( )", "%"]
+  const operatorArray = ["=", "+", "-", "*", "/"]
   return (
     <div className="h-screen w-screen bg-gray-100 dark:bg-gray-900 p-2">
       <div className="h-1/3">
@@ -9,8 +12,28 @@ export default function Home() {
           <ColorModeToggleBtn />
         </div>
       </div>
+
       <div className="h-2/3">
-        <Button>1</Button>
+        <div className="flex flex-column gap-2 justify-center">
+          <div className=" grid grid-cols-3 justify-end content-end gap-2">
+            {topOperatoryArray.map((operator, index) => (
+              <Button key={index} isOperator>
+                {operator}
+              </Button>
+            ))}
+            {numArray.map((num) => (
+              <Button key={num}>{num}</Button>
+            ))}
+          </div>
+
+          <div className="flex flex-wrap-reverse w-min content-end gap-2">
+            {operatorArray.map((operator, index) => (
+              <Button key={index} isOperator>
+                {operator}
+              </Button>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   )
