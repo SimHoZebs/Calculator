19956702ec78c1f22bd8625ba690a25b1a585072
SimHoZebs <simhozebs@gmail.com>
2021-11-14 11:01:45 +0900

    test commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="h-screen w-screen dark:bg-gray-700">
       <div className="h-1/3">Top </div>
-      <div className="h-2/3">Bottom</div>
+      <div className="h-2/3">Test</div>
     </div>
   )
 }
