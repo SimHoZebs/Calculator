3de59a38059e7ccbea649477cfc06936b607449c
SimHoZebs <simhozebs@gmail.com>
2021-11-17 21:43:49 +0900

    updated title and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,7 +17,7 @@ export default function MyApp(props: AppProps) {
         />
         <meta name="description" content="Description" />
         <meta name="keywords" content="Keywords" />
-        <title>Next.js PWA Example</title>
+        <title>Zebs&apos; calculator app</title>
 
         <link rel="manifest" href="/manifest.json" />
         <link
