005bf2b4df968a0f61f09e889d13bef15653ac00
SimHoZebs <simhozebs@gmail.com>
2021-12-08 12:34:13 -0500

    update PWA manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,18 +20,6 @@ export default function MyApp(props: AppProps) {
         <title>Zebs&apos; calculator app</title>
 
         <link rel="manifest" href="/manifest.json" />
-        <link
-          href="/icons/favicon-16x16.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-        />
-        <link
-          href="/icons/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         <meta name="theme-color" content="#317EFB" />
