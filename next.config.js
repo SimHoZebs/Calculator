8e46a53ff3a0d677ca28ac2e3c6024dc1a8038ac
Sim Ho <simhozebs@gmail.com>
2023-10-16 09:55:33 -0400

    update dependencies

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
-const withPWA = require('next-pwa')
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  runtimeCaching: true,
+  mode: 'production',
+})
 const runtimeCaching = require('next-pwa/cache')
 const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
 
 module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-    mode: 'production',
-  },
   webpack(config) {
     config.plugins.push(new WindiCSSWebpackPlugin())
     return config
